bd5f8448a73e847e5cec1d4ddbf75668494f9b00
psupri <46396932+psupri@users.noreply.github.com>
2021-09-22 16:46:55 +0530

    Update getDeployments.py

--- getDeployments.py
+++ getDeployments.py
@@ -1,8 +1,13 @@
-import requests,json
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-url = "https://api.elastic-cloud.com/api/v1/deployments"
-apikey="YWVJeDQzc0JqR3d1aWxkMTZuWFM6cWFlWGFwTk5UaWFERUkweUZPRTVNZw=="
-headers = {'Authorization': 'ApiKey ' + apikey}
-response = requests.get(url, headers=headers)
-print(json.loads(response.text))
+      import subprocess
+      import sys
+      def install(request):
+        subprocess.check_call([sys.executable, "-m", "pip", "install", request])
+      install("requests")
+      import requests,json
+      from requests.packages.urllib3.exceptions import InsecureRequestWarning
+      requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+      url = "https://api.elastic-cloud.com/api/v1/deployments"
+      apikey="YWVJeDQzc0JqR3d1aWxkMTZuWFM6cWFlWGFwTk5UaWFERUkweUZPRTVNZw=="
+      headers = {'Authorization': 'ApiKey ' + apikey}
+      response = requests.get(url, headers=headers)
+      print(json.loads(response.text))
